8e56b2674e1a4b188c9e88116d6f031a09388a1a
hazimIskandar <zim1994.hi@gmail.com>
2016-07-17 21:33:19 +0800

    add latest

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,17 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'firebase'])
 
-.factory("Gases", function($firebaseArray) {
+(function(){
+var app =  angular.module('starter', ['ionic', 'firebase'])
+
+app.factory("Gases", function($firebaseArray) {
   var itemsRef = new Firebase("https://test-d0812.firebaseio.com/Gases");
 
   return $firebaseArray(itemsRef);
 })
 
-.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider.state('landing',{
     url: '/landing',
     templateUrl: 'templates/landing.html'
@@ -36,7 +38,7 @@ angular.module('starter', ['ionic', 'firebase'])
   $urlRouterProvider.otherwise('/homepage');
 })
 
-.controller("ListCtrl", function($scope, $rootScope, Gases) {
+app.controller("ListCtrl", function($scope, $rootScope, Gases) {
 
   
 
@@ -52,6 +54,7 @@ angular.module('starter', ['ionic', 'firebase'])
   $scope.pressure= { 
     availableUnits: [
       {unit: 'Pascal'},
+      {unit: 'KPa'},
       {unit: 'Torr'},
       {unit: 'Bar'},
       {unit: 'Atmosphere'}
@@ -129,6 +132,8 @@ angular.module('starter', ['ionic', 'firebase'])
       press1 = press1 * 133.322;
     }else if (pressure.unit === 'Bar'){
       press1 = press1 * 100000;
+    }else if (pressure.unit === 'KPa'){
+      press1 = press1 * 1000;
     }else if (pressure.unit === 'Atmosphere'){
       press1 = press1 * 101325;
     };
@@ -232,7 +237,7 @@ angular.module('starter', ['ionic', 'firebase'])
 
 
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -248,4 +253,6 @@ angular.module('starter', ['ionic', 'firebase'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+}());
